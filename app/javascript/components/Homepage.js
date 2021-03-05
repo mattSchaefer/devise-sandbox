ade7af938d1b2df65774b8a6c43b60f5413c4b13
Matt Schaefer <matthew.schaefer12@gmail.com>
2021-03-04 20:52:32 -0500

    init

---
+++ app/javascript/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// var Homepage = React.createClass({
+//     render: function(){
+//         return <div>Homepage</div>
+//     }
+// })
+//
+// export default Homepage;
+
+export default () => (<div>homepage</div>);
